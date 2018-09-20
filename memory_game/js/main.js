7b8235ad88249bf240ed521a277a5cd26736e886
Grant Wilkinson <wilkinsonmgrant@gmail.com>
2018-09-20 16:00:06 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,19 +24,29 @@ var cards = [
 ];
 var cardsInPlay = [];
 var checkForMatch = function() {
+    this.setAttribute('src', cards[0].cardImage);
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	    console.log('You found a match!'); }
 	else {
 	    console.log('Sorry, try again.'); }
 }
-var flipCard = function(cardId) {
+var flipCard = function() {
+	 
 	  console.log('User flipped ' + cards[cardId].rank);
       cardsInPlay.push(cardId).rank;
       console.log(cards[cardId].cardImage);
-      console.log(cards[cardId].suit); }
-      flipCard(0);
-	  flipCard(2); 
-      checkForMatch(); 
+      console.log(cards[cardId].suit); 
+  }
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', 'i');
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById.('game-board').appendChild(node);
+}
+};
+createBoard();
        
 	   
 	
